b979d2b9f233693c1a6cb03fa283e922b1e52389
alex <spb.roout@gmail.com>
2023-10-20 16:21:51 +0300

    modify create post

--- posts/forms.py
+++ posts/forms.py
@@ -7,7 +7,7 @@ class PostModelForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('content', 'image')
+        fields = ('header', 'content', 'image')
 
 
 class CommentModelForm(forms.ModelForm):
