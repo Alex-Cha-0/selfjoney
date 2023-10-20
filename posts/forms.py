307cb8babaaf43bfc4267590b4604f8a70f572de
alex <spb.roout@gmail.com>
2023-10-20 15:44:52 +0300

    add button update and delete post

--- posts/forms.py
+++ posts/forms.py
@@ -3,6 +3,17 @@ from django import forms
 
 
 class PostModelForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 2}))
+
     class Meta:
         model = Post
         fields = ('content', 'image')
+
+
+class CommentModelForm(forms.ModelForm):
+    body = forms.CharField(label='',
+                           widget=forms.TextInput(attrs={'placeholder': 'Add a comment...', "class": "form-control"}))
+
+    class Meta:
+        model = Comment
+        fields = ('body',)
--- profiles/signals.py
+++ profiles/signals.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 
 from .models import Profile, Relationship
 
+
 @receiver(post_save, sender=User)
 def post_save_create_profile(sender, instance, created, **kwargs):
     # print('sender', sender)
@@ -11,12 +12,11 @@ def post_save_create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+
 @receiver(post_save, sender=Relationship)
 def post_save_add_to_friends(sender, instance, created, **kwargs):
     sender_ = instance.sender
     receiver_ = instance.receiver
-    print(sender_)
-    print(receiver_)
     if instance.status == 'accepted':
         sender_.friends.add(receiver_.user)
         receiver_.friends.add(sender_.user)
